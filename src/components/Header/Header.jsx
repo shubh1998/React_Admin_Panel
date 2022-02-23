dd0878491d4099fd254875427829fdf0f0df4958
piyush khodre <pkhodre@gammastack.com>
2022-02-23 15:59:28 +0530

    feat/implemented avatar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { IconButton, Toolbar, Typography } from '@mui/material'
+import { Avatar, IconButton, Menu, MenuItem, Toolbar, Typography } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
@@ -6,9 +6,40 @@ import { handleDrawerToggle } from '../../redux-thunk/redux/Ham/hamSlice'
 import { AppBar } from './header.styles'
 
 export const Header = () => {
+  const [anchorElUser, setAnchorElUser] = React.useState(null)
   const dispatch = useDispatch()
   const open = useSelector((state) => state.ham.open)
   console.log('open', open)
+
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget)
+  }
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null)
+  }
+
+  const logoutHandler = () => {
+    setAnchorElUser(null)
+    console.log('Logout')
+  }
+
+  const profileHandler = () => {
+    setAnchorElUser(null)
+    console.log('Profile')
+  }
+
+  const menuItems = [
+    {
+      label: 'Profile',
+      handler: profileHandler
+    },
+    {
+      label: 'Logout',
+      handler: logoutHandler
+    }
+  ]
+
   return (
     <AppBar position='fixed' open={open} sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: '#000000', color: '#ffffff' }}>
       <Toolbar>
@@ -21,9 +52,32 @@ export const Header = () => {
         >
           <MenuIcon />
         </IconButton>
-        <Typography variant='h6' noWrap component='div'>
+        <Typography variant='h6' noWrap component='div' sx={{ flexGrow: 1, textAlign: 'left' }}>
           Admin Panel
         </Typography>
+        <Avatar onClick={handleOpenUserMenu} sx={{ p: 0, cursor: 'pointer' }}>PK</Avatar>
+        <Menu
+          sx={{ mt: '45px' }}
+          id='menu-appbar'
+          anchorEl={anchorElUser}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right'
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right'
+          }}
+          open={Boolean(anchorElUser)}
+          onClose={handleCloseUserMenu}
+        >
+          {menuItems.map((menuItem) => (
+            <MenuItem key={menuItem.label} onClick={() => { menuItem.handler() }}>
+              <Typography textAlign='center'>{menuItem.label}</Typography>
+            </MenuItem>
+          ))}
+        </Menu>
       </Toolbar>
     </AppBar>
   )
